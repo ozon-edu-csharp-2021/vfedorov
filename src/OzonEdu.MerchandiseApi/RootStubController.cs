26eafd2d6b2ac6c887a7afb71a20335adc83b21e
Valerii Fedorov <spinball@yandex.ru>
2021-10-17 19:58:04 +0300

    Add service project skeleton

---
+++ src/OzonEdu.MerchandiseApi/RootStubController.cs
@@ -0,0 +1,24 @@
+﻿namespace OzonEdu.MerchandiseApi
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    [Route("/")]
+    [ApiController]
+    public class RootStubController : ControllerBase
+    {
+        private readonly ILogger<RootStubController> logger;
+        public RootStubController(ILogger<RootStubController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            logger.LogInformation("Stub has been called");
+
+            return "Hello from the Merchandise API!";
+        }
+    }
+}
